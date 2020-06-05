3573aef129239c9ab556434154699b7781ac74d8
adamgian <contact@adamgian.com>
2020-06-05 21:34:30 +1000

    Migrate from adobe typekit font

---
+++ src/assets/javascript/components/_webfont.js
@@ -0,0 +1,9 @@
+var WebFont = require('webfontloader');
+
+WebFont.load({
+    google: {
+        families: [
+            'Poly:400',
+        ]
+    }
+});
--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -4,9 +4,9 @@ import '../styles/scss/main.scss';
 // Javascript assets
 import './components/_analytics.js';
 import './components/_async-page-load.js';
-import './components/_typekit.js';
 // import './components/_service-worker.js';
 import './components/_vertical-rhythm.js';
+import './components/_webfont.js';
 
 
 
